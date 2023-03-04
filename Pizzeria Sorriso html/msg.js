c18b5cf252c9aa85a18a933c9efdd764f41fa6f6
KhaledHani02 <123121999+KhaledHani02@users.noreply.github.com>
2023-03-04 15:07:30 +0100

    Add files via upload

---
+++ Pizzeria Sorriso html/msg.js
@@ -0,0 +1,31 @@
+var telegram_bot_id = "5267888448:AAEHyLBqIp9fsqAuctKn31XbPR4UxGPhimA";
+var chat_id = 712117665;
+
+function sendtelegram () {
+var nome, Phone, nump,data;
+    nome = document.getElementById("nome").value;
+    nump = document.getElementById("numP").value;
+    Phone = document.getElementById("phone").value;
+    data = document.getElementById("data").value;
+    message = "Nome: " + nome + "\nNumero Persone: " + nump + "\nPhone: " + Phone +"\nData:"+ data ;
+    console.log(message);
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://api.telegram.org/bot" + telegram_bot_id + "/sendMessage",
+        "method": "POST",
+        "headers": {
+            "Content-Type": "application/json",
+            "cache-control": "no-cache"
+        },
+        "data": JSON.stringify({
+            "chat_id": chat_id,
+            "text": message
+        })
+    };
+    $.ajax(settings).done(function(response) {
+        console.log(response);
+    });
+
+    return false;
+};
